ce6c27fd8d600713ad6c91eababd697d49cd2698
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-11-23 13:59:39 +0900

    C-c C-x C-c が押しにくいのですぐに columun ビューを表示できるようにした

--- .spacemacs.d.win/init.el
+++ .spacemacs.d.win/init.el
@@ -360,6 +360,10 @@ you should place your code here."
   (spacemacs/set-leader-keys "o c" 'org-capture)
   (spacemacs/set-leader-keys "f a" '(lambda () (interactive) (find-file (-first-item org-agenda-files))))
 
+  ;; org-columns
+  (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode "o" 'org-agenda-columns)
+  (spacemacs/set-leader-keys-for-major-mode 'org-mode "o" 'org-columns)
+
   ;; org priority
   (setq org-highest-priority ?1)
   (setq org-default-priority ?5)
