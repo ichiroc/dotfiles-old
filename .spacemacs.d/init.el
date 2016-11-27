63d140a8a02b5aa53c35b2f809e4ac323e1d87f9
NAKATANI Ichiro <nobitahouse@gmail.com>
2016-11-28 07:44:56 +0900

    markdown のインデントの空白を自分の習慣の 2 と合わせた

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -469,6 +469,7 @@ $0")
 
   ;; markdown
   (setq markdown-command "markdown2")
+  (setq markdown-list-indent-width 2)
   (setq markdown-xhtml-header-content
         "<link href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\"
                rel=\"stylesheet\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\"
